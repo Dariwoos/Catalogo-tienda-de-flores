cf0296afc97d543dc89583d163f2ff1aa555d067
Mohamad Alzaim <tst.info20@gmail.com>
2022-04-04 00:00:33 +0200

    Actualizado

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Card.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Card.css';
 const Card = (props) => {
   return (
-    <div className="row ">
+    <div className='row '>
       {props.Flores.filter((value) => {
-        if (value.name === "") {
+        if (value.name === '') {
           return value;
         } else if (
           value.name.toLowerCase().includes(props.busqueda.toLowerCase())
         ) {
           return (
-            <div className="card col-4  ">
-              <img src={value.imgUrl} className="card-img-top " alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">{value.name} </h5>
-                <p className="card-text"></p>
+            <div className='card col-4  '>
+              <img src={value.imgUrl} className='card-img-top ' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>{value.name} </h5>
+                <p className='card-text'></p>
                 <Link
-                  to="/detalles/{value.id} "
+                  to={`/detalles/${value.id}`}
                   state={{ from: value.id }}
-                  className="btn btn-primary"
+                  className='btn btn-primary'
                 >
                   Go somewhere
                 </Link>
@@ -29,15 +29,15 @@ const Card = (props) => {
         }
       }).map((value, key) => {
         return (
-          <div className="card col-4 " key={key}>
-            <img src={value.imgUrl} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{value.name} </h5>
-              <p className="card-text"></p>
+          <div className='card col-4 ' key={key}>
+            <img src={value.imgUrl} className='card-img-top' alt='...' />
+            <div className='card-body'>
+              <h5 className='card-title'>{value.name} </h5>
+              <p className='card-text'></p>
               <Link
                 to={`/detalles/${value.id}`}
                 state={{ from: value.id }}
-                className="btn btn-primary"
+                className='btn btn-primary'
               >
                 Go somewhere
               </Link>
